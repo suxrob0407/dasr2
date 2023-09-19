fd76256743ee597466ee69188bc7ac9d6f60da70
unknown <ondamurodovsukhrob0716@gmail.com>
2023-09-19 16:43:40 +0500

    dars2

---
+++ main.js
@@ -0,0 +1,221 @@
+//       let odamlar=[
+//     {
+//        id:1586,
+//        name:"suxrob",
+//        jinsi:'erkak',
+//        yoshi:19,
+//      },
+
+//     {
+//        id:1576,
+//        name:"zulfira",
+//        jinsi:'ayol',
+//        yoshi:15,
+//      },
+
+//     {
+//        id:1596,
+//        name:"mirshod",
+//        jinsi:'erkak',
+//        yoshi:9,
+//      },
+
+//     {
+//        id:1456,
+//        name:"sevinch",
+//        jinsi:'ayol',
+//        yoshi:20,
+//      },
+
+//     {
+//        id:1472,
+//        name:"moxinur",
+//        jinsi:'ayol',
+//        yoshi:14,
+//      },
+
+//     {
+//        id:5546,
+//        name:"diyor",
+//        jinsi:'erkak',
+//        yoshi:17,
+//      },
+
+//     {
+//        id:7552,
+//        name:"shoxrux",
+//        jinsi:'erkak',
+//        yoshi:39,
+//      },
+
+//     {
+//        id:8921,
+//        name:"bobur",
+//        jinsi:'erkak',
+//        yoshi:25,
+//      },
+//      {
+//        id: 7895,
+//        name:"marjona",
+//        jinsi:'ayol',
+//        yoshi:21,
+//      },
+//      {
+//        id:7536 ,
+//        name:"rustam",
+//        jinsi:'erkak',
+//        yoshi:16,
+//      },
+//      {
+//        id:9878 ,
+//        name:"madina",
+//        jinsi:'ayol',
+//        yoshi:33,
+//      },
+//      {
+//        id: 5544,
+//        name:"shaxina",
+//        jinsi:'ayol',
+//        yoshi:17,
+//      },
+//      {
+//        id: 4444,
+//        name:"erkin",
+//        jinsi:'erkak',
+//        yoshi:22,
+//      },
+//      {
+//        id: 1212,
+//        name:"olim",
+//        jinsi:'erkak',
+//        yoshi:32,
+//      },
+//      {
+//        id:1483 ,
+//        name:"mushtariy",
+//        jinsi:'ayol',
+//        yoshi:53,
+//      },
+//      {
+//        id: 3984,
+//        name:"abubakir",
+//        jinsi:'erkak',
+//        yoshi:47,
+//      },
+//      {
+//        id:4157 ,
+//        name:"suxrob",
+//        jinsi:'erkak',
+//        yoshi:17,
+//      },
+//      {
+//        id:1981 ,
+//        name:"muhammadrizo",
+//        jinsi:'erkak',
+//        yoshi:69,
+//      },
+//      {
+//        id:1981 ,
+//        name:"muhammadrizo",
+//        jinsi:'erkak',
+//        yoshi:69,
+//      },
+//      {
+//        id:7894 ,
+//        name:"muhammad",
+//        jinsi:'erkak',
+//        yoshi:39,
+//      },
+//      {
+//        id:78965 ,
+//        name:"gulxayo",
+//        jinsi:'ayol',
+//        yoshi:34,
+//      },
+//      {
+//        id: 8947,
+//        name:"lochin",
+//        jinsi:'ayol',
+//        yoshi:36,
+//      },
+//      {
+//        id: 4568,
+//        name:" elnur",
+//        jinsi:'erkak',
+//        yoshi:19,
+//      },
+//      {
+//        id:3654 ,
+//        name:"quvonch",
+//        jinsi:'erkak',
+//        yoshi:17,
+//      },
+//      {
+//        id:7823 ,
+//        name:"elmar",
+//        jinsi:'erkak',
+//        yoshi:21,
+//      },
+//      {
+//        id:8795 ,
+//        name:"elchin",
+//        jinsi:'erkak',
+//        yoshi:23,
+//      },
+//      {
+//        id: 5782,
+//        name:"gulnur",
+//        jinsi:'ayol',
+//        yoshi:27,
+//      },
+//      {
+//        id:1897 ,
+//        name:"asilbek",
+//        jinsi:'erkak',
+//        yoshi:25,
+//      },
+//   ];
+//  if (odamlar[25].id==4586) {
+//      console.log('ayol');
+//  }else{
+//   console.log('erkak');
+//  }
+
+
+// let student = [
+//   {
+//     id: 18586,
+//     name: "suxrob",
+//     gender: "male",
+//   },
+//   {
+//     id: 18686,
+//     name: "zulfira",
+//     gender: "female",
+//   },
+// ];
+// if (student[1].gender=='male') {
+//     console.log('erkak');
+// }else{
+//     console.log('ayol');
+// }
+  
+// let weerk =prompt('ixtiyoriy 1dan 7 gacha son kiriting')
+// if (weerk==1) {
+//   console.log('dushanba');
+// } else if(weerk==2) {
+//   console.log('seshanba');
+// }else if(weerk==3){
+//   console.log('chorshanba');
+// }else if(weerk==4){
+//   console.log('payshanba');
+// }else if(weerk==5){
+//   console.log('juma');
+// }else if(weerk==6){
+//   console.log('shanba');
+// }else if(weerk==7){
+//   console.log('yakshanba');
+// }else{
+//   console.log('bunday hafta yoq');
+// }
+// console.log(weerk);
